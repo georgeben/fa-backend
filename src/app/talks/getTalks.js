ff1b39b79ee3a166bcb0499bae52c32e28ced801
georgeben <msdcconnect@gmail.com>
2022-02-21 12:58:26 +0100

    feat: sort talks by most recent

--- src/app/talks/getTalks.js
+++ src/app/talks/getTalks.js
@@ -1,5 +1,5 @@
 function getTalks({ talkRepository }) {
-  return () => talkRepository.find({}, {}, { lean: true, populate: "speaker" }, true);
+  return () => talkRepository.find({}, {}, { lean: true, populate: "speaker", sort: { createdAt: -1 } }, true);
 }
 
 export default getTalks;
